37001c065e8a44e5be3ef2764f37008479ecc1e6
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-03-28 11:28:21 +0530

    Login, Signup, Home and Welcome screens component created

---
+++ Application1/screens/SignUpScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+function SignUpScreen() {
+  return (
+    <View>
+      <Text> SignUpScreen </Text>
+    </View>
+  )
+}
+
+export default SignUpScreen
---
+++ Application1/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+function WelcomeScreen() {
+  return (
+    <View>
+      <Text> WelcomeScreen </Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
