f4c62f85a572d81c5ad11ebdafd64d601d3db53d
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-03-29 11:45:57 +0530

    Recipe Screen created

---
+++ Application2/src/screens/RecipeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+const RecipeScreen = () => {
+  return (
+    <View>
+      <Text>RecipeList Screen</Text>
+    </View>
+  )
+}
+
+export default RecipeScreen
