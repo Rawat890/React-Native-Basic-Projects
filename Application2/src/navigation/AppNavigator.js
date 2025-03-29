5f50953d805c5f61177208a84c076cb652ff36cf
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-03-29 11:45:28 +0530

    Recipe List Navigation screen registered

--- Application2/src/navigation/AppNavigator.js
+++ Application2/src/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import WelcomeScreen from '../screens/WelcomeScreen';
+import RecipeScreen from '../screens/RecipeScreen';
 
 
 const stack = createNativeStackNavigator();
@@ -11,6 +12,7 @@ const AppNavigator = () => {
     <NavigationContainer>
       <stack.Navigator screenOptions={{ headerShown: false }}>
         <stack.Screen component={WelcomeScreen} name='Welcome' />
+        <stack.Screen component={RecipeScreen} name='Recipe' />
       </stack.Navigator>
 
     </NavigationContainer>
