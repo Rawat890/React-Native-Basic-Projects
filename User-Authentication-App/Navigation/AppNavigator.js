03877901f9c861e3421745d067c29db7544bc702
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-04-01 12:20:01 +0530

    App Navigator created

---
+++ User-Authentication-App/Navigation/AppNavigator.js
@@ -0,0 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LogInScreen from '../screens/LogInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: true
+      }}>
+        <Stack.Screen component={LogInScreen} name='Login' />
+        <Stack.Screen component={SignUpScreen} name='SignUp' />
+        <Stack.Screen component={WelcomeScreen} name='Welcome' />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default AppNavigator
