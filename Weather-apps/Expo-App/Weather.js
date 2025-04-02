250c78fcecef243abe98f0ae08c3b463331c2d99
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-04-02 11:39:10 +0530

    Wreather.js created

---
+++ Weather-apps/Expo-App/Weather.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import * as Location from 'expo-status-bar'
+import { View, Text } from 'react-native'
+
+
+const Weather = () => {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true)
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+export default Weather
